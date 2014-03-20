c3f3fd261af1962d8dd47572da6978d6674d6dda
Michael Shimniok <shimniok@gmail.com>
2014-03-20 19:27:02 +0000

    Revised to allow interactive time setting. Finished automatic Daylight Savings Time.

--- DayClock/DayClock.ino
+++ DayClock/DayClock.ino
@@ -4,9 +4,12 @@
 #include <LedControl.h>
 
 //MCP79412RTC clock;
-LedControl lc=LedControl(11,13,10,3);
+LedControl lc=LedControl(12,11,10,3);
+boolean dst = false;
 
 #define MAXROW 8      // maximum number of LED rows
+
+#define TICK 500      // amount of time in ms that the second : stays lit
 
 #define SUN 0
 #define MON 1
@@ -307,18 +310,24 @@ void setup() {
     /* and clear the display */
     lc.clearDisplay(address);
   }
+  
+  // Turn on RTC
+  DDRC |= (1<<PC3)|(1<<PC2);
+  PORTC |= (1<<PC3);
 
   Serial.begin(9600);
+  Serial.println("DayClock v1.0 Michael Shimniok www.bot-thoughts.com");
   while (1) {
     setSyncProvider(RTC.get);   //the function to get the time from the RTC
     Serial.print("RTC SYNC");
     if (timeStatus() == timeSet) break;
-    Serial.print(" FAIL");   
+    Serial.println(" FAIL");   
     lc.setLed(2,0,0,true);
     delay(200);
     lc.setLed(2,0,0,false);
     delay(200);
   }
+  Serial.println(" OK");    
   Serial.println();
 }
 
@@ -389,28 +398,163 @@ void drawClock(int hour, int minute, boolean showColon) {
   }
 }
 
+int lastMin = -1;
 int lastSec = -1;
 
-/*
-// Test days
-void loop() {
-  for (int d=1; d <= 7; d++) {
-    drawDay(d);
-    delay(2000);
+/* isDST
+ * 
+ * Based on USA DST observation as of 2013, starts
+ * 2nd Sunday of March, 2:00am, stops 1st Sunday of
+ * November, 2:00am.
+ */
+boolean isDST(time_t t) {
+  boolean dst = false;
+  int mo = month(t);
+  int d = day(t);
+  int xday = day(t)-weekday(t); 
+  
+  if (mo > 3 && mo < 11) {
+    dst = true;
+  } else if (mo == 3) {
+    if (xday >= 7) {
+      dst = true;
+    } else if (xday >= 0) {
+      if (weekday(t) > 1) {
+        dst = true;
+      } else if (hour(t) >= 2) {
+        dst = true;
+      }
+    }
+  } else if (mo == 11) {
+    
+    if (xday >= 14) {
+      dst = true;
+    } else if (xday >= 7) {
+      if (weekday(t) > 1) {
+        dst = true;
+      } else if (hour(t) >= 2) {
+        dst = true;
+      }
+    }
   }
+  
+  return dst;
 }
-*/
+
+void flushInput(void)
+{
+    do {
+        delay(2);
+        Serial.read();
+    } while (Serial.available() > 0);
+}
+
+//Read command from the Arduino serial monitor to set the RTC.
+//Case-sensitive and must be entered exactly as (24-hour clock):
+//  Set yyyy-mm-dd hh:mm:ss
+void readCommand()
+{
+    char cmd[24] = "Set yyyy-mm-dd hh:mm:ss";
+    static int i;
+    tmElements_t tmSet;
+    time_t tSet;
+
+    if (Serial.available() >= 23) {              //enough characters for the whole command?
+        i = 0;                                   //yes, read the available characters
+        while (Serial.available() > 0) {
+            if (i >= sizeof(cmd) - 1) {          //more than we can enjoy
+                flushInput();                    //clear out the input buffer
+                Serial.print("Too long: ");
+                Serial.println(cmd);
+                return;
+            }
+            delay(2);                            //let the next character trickle in
+            cmd[i++] = char(Serial.read());
+        }
+        cmd[i] = 0;                              //put in string terminator
+
+        if (strncmp(cmd, "Set ", 4) == 0) {
+            tmSet.Year = 1000 * (cmd[4] - '0') + 100 * (cmd[5] - '0') + 10 * (cmd[6] - '0') + cmd[7] - '0' - 1970;
+            tmSet.Month = 10 * (cmd[9] - '0') + cmd[10] - '0';
+            tmSet.Day = 10 * (cmd[12] - '0') + cmd[13] - '0';
+            tmSet.Hour = 10 * (cmd[15] - '0') + cmd[16] - '0';
+            tmSet.Minute = 10 * (cmd[18] - '0') + cmd[19] - '0';
+            tmSet.Second = 10 * (cmd[21] - '0') + cmd[22] - '0';
+            tSet = makeTime(tmSet);		 //convert to time_t
+            // Correct for DST
+            if (isDST(tSet)) {
+              tmSet.Hour -= 1;
+              tSet = makeTime(tmSet);		 //convert to time_t
+            }
+            setTime(tSet);			 //set the system time
+            RTC.set(now());		         //set the rtc
+            Serial.println("RTC set!");
+            flushInput();                        //discard any extraneous trailing characters
+        }
+        else {
+            Serial.print("Unknown: ");
+            Serial.println(cmd);
+        }
+    }
+}
+
+
+//Print an integer in "00" format (with leading zero),
+//followed by a delimiter.
+//Input value assumed to be between 0 and 99.
+void printI00(int val, char delim)
+{
+    if (val < 10) Serial.print('0');
+    Serial.print(val);
+    Serial.print(delim);
+    return;
+}
+
+//Print time (and date) given a time_t value
+void printTime(time_t t)
+{
+    int h = hour(t);
+    if (isDST(t))
+      h += 1;
+    printI00(h, ':');
+    printI00(minute(t), ':');
+    printI00(second(t), ' ');
+    Serial.print(dayShortStr(weekday(t)));
+    Serial.print(' ');
+    printI00(day(t), ' ');
+    Serial.print(monthShortStr(month(t)));
+    Serial.print(' ');
+    Serial.println(year(t));
+}
 
 void loop() {
   time_t time = now();
+  int h = hour(time);
+  int m = minute(time);
+
+  // Correct time for DST
+  if (isDST(time)) {
+    h += 1;
+  }
+
+  readCommand();
+    
+  if (lastMin != minute(time)) {
+    printTime(time);   
+    Serial.println("To set the time: Set yyyy-mm-dd hh:mm:ss");
+    lastMin = minute(time);
+  }
 
   if (lastSec != second(time)) {
     if (20 <= second(time) && second(time) < 40) {
       drawDay(weekday(time));
     } else {
-      drawClock(hour(time), minute(time), (second(time)&1));
+      drawClock(h, m, true);
+      delay(TICK);
+      drawClock(h, m, false);
     }
     lastSec = second(time);
+
   }
   delay(100);
 }
