ab465641973ae24bdfc3efbefb8934611f303fdf
shimniok@gmail.com <shimniok@gmail.com>
2016-01-04 16:42:58 -0700

    Revised and hopefully fixed DST computation

--- DayClock/firmware/DayClock/DST.ino
+++ DayClock/firmware/DayClock/DST.ino
@@ -1,40 +1,55 @@
-/* isDST
- * 
- * Based on USA DST observation as of 2013, starts
- * 2nd Sunday of March, 2:00am, stops 1st Sunday of
- * November, 2:00am.
+/** Determine if Daylight Savings Time is active or not.
+ *
+ * based on: http://stackoverflow.com/questions/5590429/calculating-daylight-savings-time-from-only-date
+ *
+ *   This is actually deceptively simple. There are a few facts that will help us:
+ *   In most of the US, DST starts on the second Sunday of March and ends on the first Sunday of
+ *   November, at 2:00 AM both times.
+ *
+ *   The second Sunday in March will always be between the 8th and the 14th inclusive.
+ *   The first Sunday in November will always be between the 1st and 7th inclusive.
+ *   The day of week numbering is quite convenient because the day - day-of-week will give you
+ *      the previous Sunday.
+ *
+ * INPUT:
+ *   mo - month, 1-12
+ *   d  - date, 1-31
+ *   wd - day of week, 1-7
  */
-boolean isDST(time_t t) {
+boolean isDST(int mo, int d, int wd, int hr) {
   boolean dst = false;
-  int mo = month(t);
-  int d = day(t);
-  int xday = day(t)-weekday(t); 
-  
-  if (mo > 3 && mo < 11) {
+  int prevSunday = d - wd + 1;
+
+  if (mo >= Apr && mo <= Oct) {    // April thru October are definitely DST
     dst = true;
-  } else if (mo == 3) {
-    if (xday >= 7) {
-      dst = true;
-    } else if (xday >= 0) {
-      if (weekday(t) > 1) {
+  } else if (mo == Mar) {          // March might be DST...
+    if (prevSunday <= 7) {         // No DST if first Sunday
+      dst = false;
+    } else if (prevSunday <= 14) { // if Second Sunday...
+      if (wd > Sun) {              // yes if > Sunday
         dst = true;
-      } else if (hour(t) >= 2) {
-        dst = true;
+      } else {                     // yes, if Sunday, after 2am...
+        dst = hr >= 2;
       }
-    }
-  } else if (mo == 11) {
-    
-    if (xday >= 14) {
+    } else {                       // Yes DST if third Sunday or later
       dst = true;
-    } else if (xday >= 7) {
-      if (weekday(t) > 1) {
-        dst = true;
-      } else if (hour(t) >= 2) {
-        dst = true;
-      }
     }
+  } else if (mo == Nov) {          // Nov might be DST...
+    if (prevSunday < 1) {          // Yes if before first Sun
+      dst = true;
+    } else if (prevSunday < 14) {  // if first Sunday...
+      if (wd > Sun) {              // no if > Sunday, after 2am...
+        dst = false;
+      } else {
+        dst = hr < 2;              // if Sunday, before 2am
+      }
+    } else {                       // No DST if 2nd Sunday or later...
+      dst = false;
+    }
+
+  } else {
+    dst = false;
   }
-  
+    
   return dst;
 }
-
