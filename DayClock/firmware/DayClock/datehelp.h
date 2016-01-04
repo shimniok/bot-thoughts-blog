a5d186e2048389ef6cce93da6c124e7b7a72c44a
shimniok@gmail.com <shimniok@gmail.com>
2016-01-04 16:42:20 -0700

    moved date numerics/enums into separate header

---
+++ DayClock/firmware/DayClock/datehelp.h
@@ -0,0 +1,10 @@
+enum days { Sun=1, Mon, Tue, Wed, Thu, Fri, Sat };
+
+enum months { Jan=1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+
+enum letters { LETTER_A=13, LETTER_B, LETTER_C, LETTER_D, LETTER_E, LETTER_F, LETTER_G,
+  LETTER_H, LETTER_I, LETTER_J, LETTER_L, LETTER_M, LETTER_N, LETTER_O, LETTER_P, 
+  LETTER_R, LETTER_S, LETTER_T, LETTER_U, LETTER_V, LETTER_W, LETTER_Y,
+  LETTER_COLON, LETTER_BLANK };
+
+
